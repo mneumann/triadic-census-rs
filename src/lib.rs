1dd09f3bd360d7d4484b3eb39e4ed2502c48aad9
Michael Neumann <mneumann@ntecs.de>
2015-10-27 12:03:31 +0100

    Base implementation on trait DirectedGraph instead of using PetGraph directly
    This allows more efficient implementations of e.g. find_edge() (using a adjacency matrix)

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ extern crate petgraph;
 use petgraph::{Graph, Directed};
 use petgraph::graph::{NodeIndex, IndexType};
 use std::collections::HashSet;
-use std::hash::Hash;
 
 #[repr(u8)]
 #[derive(Debug)]
@@ -40,28 +39,38 @@ impl TriadType {
     }
 }
 
-fn tricode<N, E, I: IndexType>(graph: &Graph<N, E, Directed, I>,
-                               v: NodeIndex<I>,
-                               u: NodeIndex<I>,
-                               w: NodeIndex<I>)
+pub type NodeIdx = usize;
+
+/// Interface required for Triadic Census.
+/// Nodes are externally represented by `NodeIdx`.
+pub trait DirectedGraph {
+    fn node_count(&self) -> NodeIdx;
+    fn has_edge(&self, src: NodeIdx, dst: NodeIdx) -> bool;
+    fn each_undirected_neighbor<F:FnMut(NodeIdx)>(&self, node: NodeIdx, callback: F);
+}
+
+fn tricode<G: DirectedGraph>(graph: &G,
+                               v: NodeIdx,
+                               u: NodeIdx,
+                               w: NodeIdx)
                                -> usize {
     let mut tricode = 0;
-    if let Some(_) = graph.find_edge(v, u) {
+    if graph.has_edge(v, u) {
         tricode |= 1 << 0;
     }
-    if let Some(_) = graph.find_edge(u, v) {
+    if graph.has_edge(u, v) {
         tricode |= 1 << 1;
     }
-    if let Some(_) = graph.find_edge(v, w) {
+    if graph.has_edge(v, w) {
         tricode |= 1 << 2;
     }
-    if let Some(_) = graph.find_edge(w, v) {
+    if graph.has_edge(w, v) {
         tricode |= 1 << 3;
     }
-    if let Some(_) = graph.find_edge(u, w) {
+    if graph.has_edge(u, w) {
         tricode |= 1 << 4;
     }
-    if let Some(_) = graph.find_edge(w, u) {
+    if graph.has_edge(w, u) {
         tricode |= 1 << 5;
     }
     return tricode;
@@ -122,34 +131,23 @@ impl TriadicCensus {
     }
 
     /// XXX: Optimize: find_edge() is O(k) with k the number of edges.
-    pub fn from_graph<N, E, I: IndexType + Hash>(graph: &Graph<N, E, Directed, I>) -> TriadicCensus {
+    pub fn from_graph<G: DirectedGraph>(graph: &G) -> TriadicCensus {
         let n = graph.node_count();
 
         let mut census = TriadicCensus::new();
 
-        for i in 0..n {
-            let v = NodeIndex::new(i);
-
+        for v in 0..n {
             let mut neighbors_v = HashSet::new();
-            for u in graph.neighbors_undirected(v) {
-                if v < u {
-                    neighbors_v.insert(u);
-                }
-            }
+            graph.each_undirected_neighbor(v, |u| if u > v { neighbors_v.insert(u); });
 
             for u in neighbors_v {
-                let mut s = HashSet::new();
-                for nu in graph.neighbors_undirected(u) {
-                    s.insert(nu);
-                }
-                for nv in graph.neighbors_undirected(v) {
-                    s.insert(nv);
-                }
+                let mut s = HashSet::new(); // XXX: Use bitset? // reuse
+                graph.each_undirected_neighbor(u, |nu| {s.insert(nu);});
+                graph.each_undirected_neighbor(v, |nv| {s.insert(nv);});
                 let _ = s.remove(&u);
                 let _ = s.remove(&v);
 
-                let tri_type = if graph.find_edge(v, u).is_some() &&
-                                  graph.find_edge(u, v).is_some() {
+                let tri_type = if graph.has_edge(v, u) && graph.has_edge(u, v) {
                     TriadType::T102
                 } else {
                     TriadType::T012
@@ -161,7 +159,7 @@ impl TriadicCensus {
                 for w in s {
                     if u < w ||
                        (v < w && w < u &&
-                        !(graph.find_edge(v, w).is_some() || graph.find_edge(w, v).is_some())) {
+                        !(graph.has_edge(v, w) || graph.has_edge(w, v))) {
                         let tri_type = TriadType::from_u8(TRITYPES[tricode(graph, v, u, w)]);
                         census.add(tri_type, 1);
                     }
@@ -180,19 +178,55 @@ impl TriadicCensus {
     }
 }
 
+pub struct SimpleDiGraph {
+    g: Graph<(), (), Directed>
+}
+
+impl SimpleDiGraph {
+    pub fn new() -> SimpleDiGraph {
+        SimpleDiGraph {g: Graph::new()}
+    }
+
+    pub fn add_node(&mut self) -> NodeIdx {
+        self.g.add_node(()).index()
+    }
+
+    pub fn add_edge(&mut self, src: NodeIdx, dst: NodeIdx) {
+        self.g.add_edge(NodeIndex::new(src), NodeIndex::new(dst), ());
+    }
+}
+
+impl DirectedGraph for SimpleDiGraph {
+    fn node_count(&self) -> NodeIdx {
+        self.g.node_count()
+    }
+
+    #[inline]
+    fn has_edge(&self, src: NodeIdx, dst: NodeIdx) -> bool {
+        self.g.find_edge(NodeIndex::new(src), NodeIndex::new(dst)).is_some()
+    }
+
+    #[inline]
+    fn each_undirected_neighbor<F:FnMut(NodeIdx)>(&self, node: NodeIdx, mut callback: F) {
+        for n in self.g.neighbors_undirected(NodeIndex::new(node)) {
+            callback(n.index())
+        }
+    }
+}
+
 #[test]
 fn test_simple() {
-    let mut g: Graph<(), ()> = Graph::new();
+    let mut g = SimpleDiGraph::new();
 
-    let n1 = g.add_node(());
-    let n2 = g.add_node(());
-    let n3 = g.add_node(());
+    let n1 = g.add_node();
+    let n2 = g.add_node();
+    let n3 = g.add_node();
 
     let c1 = TriadicCensus::from_graph(&g);
     assert_eq!(&[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                c1.as_slice());
 
-    g.add_edge(n1, n3, ());
+    g.add_edge(n1, n3);
     let c2 = TriadicCensus::from_graph(&g);
     assert_eq!(&[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                c2.as_slice());
@@ -200,42 +234,42 @@ fn test_simple() {
     let d = TriadicCensus::distance(&c1, &c2); // should be sqrt(2)
     assert!(d > 1.414 && d < 1.415);
 
-    g.add_edge(n3, n1, ());
+    g.add_edge(n3, n1);
     let c3 = TriadicCensus::from_graph(&g);
     assert_eq!(&[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                c3.as_slice());
 
-    g.add_edge(n2, n3, ());
+    g.add_edge(n2, n3);
     let c4 = TriadicCensus::from_graph(&g);
     assert_eq!(&[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                c4.as_slice());
 }
 
 #[cfg(test)]
-fn line_graph(n: u32) -> Graph<(), (), Directed> {
-    let mut g: Graph<(), (), Directed> = Graph::new();
+fn line_graph(n: u32) -> SimpleDiGraph {
+    let mut g = SimpleDiGraph::new();
 
-    let mut prev = g.add_node(());
+    let mut prev = g.add_node();
     for _ in 0..n - 1 {
-        let cur = g.add_node(());
-        g.add_edge(prev, cur, ());
+        let cur = g.add_node();
+        g.add_edge(prev, cur);
         prev = cur;
     }
     return g;
 }
 
 #[cfg(test)]
-fn circular_graph(n: u32) -> Graph<(), (), Directed> {
-    let mut g: Graph<(), (), Directed> = Graph::new();
+fn circular_graph(n: u32) -> SimpleDiGraph {
+    let mut g = SimpleDiGraph::new();
 
-    let first = g.add_node(());
+    let first = g.add_node();
     let mut prev = first;
     for _ in 0..n - 1 {
-        let cur = g.add_node(());
-        g.add_edge(prev, cur, ());
+        let cur = g.add_node();
+        g.add_edge(prev, cur);
         prev = cur;
     }
-    g.add_edge(prev, first, ());
+    g.add_edge(prev, first);
     return g;
 }
 
